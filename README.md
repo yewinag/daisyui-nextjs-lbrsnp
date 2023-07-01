3fdf5c49cb20820fb8c121c9577a764e73893013
yewinag <yewinaung831@gmail.com>
2023-07-01 11:36:54 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# daisyui-nextjs-lbrsnp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/daisyui-nextjs-lbrsnp)
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div>
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-secondary">Button</button>
+      <button className="btn btn-accent">Button</button>
+      <button className="btn btn-ghost">Button</button>
+      <button className="btn btn-link">Button</button>
+    </div>
+  );
+}
